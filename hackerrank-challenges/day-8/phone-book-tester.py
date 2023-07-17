692257691bd8f6430f083855110de36df34f969e
Eduardo Silva <eduardoalsilva45@gmail.com>
2023-07-17 01:08:16 -0300

    Adicionando a pasta hackerrank-challenges

---
+++ hackerrank-challenges/day-8/phone-book-tester.py
@@ -0,0 +1,10 @@
+file = open(phone-book-input.txt)
+
+lines_number = 0
+for line in file:
+    lines_number += 1
+    l.append().split()
+
+file = open()
+
+print(f'Number of lines: {lines_number}')
---
+++ hackerrank-challenges/day-8/phone-book.py
@@ -0,0 +1,30 @@
+# n = int(input())
+phone_book = {}
+
+# for i in range(n):
+#     l = input().split()
+#     if len(l) > 1:
+#         phone_book[l[0]] = l[1]
+
+l = []
+# item = input()
+
+file = list(open('phone-book-input.txt'))
+print(file)
+for line in range(len(file)):
+    if line == 0:
+        n = line
+    elif i in range(n):
+        l = file[line].split()
+        if len(l) > 1:
+            phone_book[l[0]] = l[1]
+
+# while item != '':
+#     l.append(item)
+#     item = input()
+
+# for i in range(len(l)):
+#     if l[i] in phone_book.keys():
+#         print(f'{l[i]}={phone_book[l[i]]}')
+#     else:
+#         print('Not found')
